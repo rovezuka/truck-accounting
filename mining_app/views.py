c59e6be5e83927855ef3a06948b8026a27c66075
rovezuka <rostislav.04@list.ru>
2024-06-13 01:13:51 +0300

    add forms for coordinates

--- mining_app/views.py
+++ mining_app/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Truck, Warehouse
+import numpy as np
 # Create your views here.
 
 
 def truck_list(request):
     trucks = Truck.objects.all()
     warehouses = Warehouse.objects.all()
+
+    coords = request.POST.getlist("coords")
+    
+    # TODO: выполнить проверки на координаты
+        
+    for coord in coords:
+        x, y = [int(i) for i in coord.split()]
+        print(x, y)
+
     context = {
         "trucks": trucks,
         "warehouses": warehouses,
     }
+    
     return render(request, "mining_app/truck_list.html", context)
